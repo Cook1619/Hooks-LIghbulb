1ad7711434d535806484c5a9c2b763848c31e480
MCook <cook6348@gmail.com>
2021-01-28 19:45:14 -0600

    Adding logic for more robust app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import shortid from "shortid";
 import "./styles.css";
 // Components
 import Light from "./Light";
+import { MdAdd } from "react-icons/md";
 
 export default function App() {
+  const [lights, setLights] = useState([]);
+  
+  const addLight = () => {
+    const id = shortid.generate(); // DO NOT EDIT THIS LINE
+    setLights((light) => [
+      ...light,
+      {
+        id,
+        on: false
+      }
+    ])
+  };
+
+  const removeLight = (id) => {
+    if(id){
+      setLights((lights) => lights.filter((light) => light.id !== id))
+    }
+  };
+
+  const toggleLight = (id) => {
+    if(id){
+      setLights((lights) => (
+        lights.map((light) => (
+          light.id === id ? {...light, on: !light.on } : light
+        ))
+      ))
+    }
+  };
+
+  // TODO: implement this function
+  const renderLights = () => (
+    <div className="lights">{lights.map(({id, on}) => (
+      <Light key={id} id={id} on={on} onClickRemove={removeLight} onClickLight={toggleLight}/>
+    ))}</div>
+  );
+
   return (
     <div className="app">
       <header>
+        <img src="alterclass.png" alt="AlterClass" width="60px" height="60px" />
         <h1>Smart Lighting</h1>
       </header>
-      <Light />
+      <div className="actions">
+        <div className="action" title="Add light" onClick={addLight}>
+          <MdAdd />
+        </div>
+      </div>
+      {renderLights()}
     </div>
   );
 }
--- src/Light.js
+++ src/Light.js
@@ -1,23 +1,38 @@
-import React, { useState } from "react";
-import { networkDelay } from "./utils";
+import React from "react";
+import PropTypes from "prop-types";
 // Components
 import { FaLightbulb } from "react-icons/fa";
+import { MdRemoveCircleOutline } from "react-icons/md";
 
-const Light = () => {
-  const [on, setOn] = useState(false);
-
-  // TODO: edit this function
-  const toggleLight = async () => {
-    await networkDelay(); // DO NOT EDIT THIS LINE
-    setOn((on) => !on)
-  };
+const Light = ({
+  id,
+  on = false,
+  onClickLight = () => null,
+  onClickRemove = () => null
+}) => {
+  if (!id) return null;
 
   return (
-    <FaLightbulb
-      onClick={toggleLight}
-      className={`light ${on ? "light-on" : "light-off"}`}
-    />
+    <div className="light-container">
+      <FaLightbulb
+        onClick={() => onClickLight(id)}
+        className={`light ${on ? "light-on" : "light-off"}`}
+      />
+      <span>ID: {id}</span>
+      <MdRemoveCircleOutline
+        className="remove"
+        title="Remove light"
+        onClick={() => onClickRemove(id)}
+      />
+    </div>
   );
+};
+
+Light.propTypes = {
+  id: PropTypes.string.isRequired,
+  on: PropTypes.bool,
+  onClickLight: PropTypes.func,
+  onClickRemove: PropTypes.func
 };
 
 export default Light;
